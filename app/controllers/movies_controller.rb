2e6f261ae5bef565ea2152fb0ac60505ea9f1f53
Philip <philipmjobe@gmail.com>
2022-09-12 15:53:59 -0400

    tries to implement reddit help to no avail

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,8 @@
 class MoviesController < ApplicationController
  
   def index
-    movie_arr = []
-    (1..100).each do |page|
-      movie_resp = HTTP.get("https://api.themoviedb.org/3/discover/movie?api_key=GOESHERE&language=en-US&sort_by=release_date.desc&page=#{page}&with_genres=27") 
-      movie_arr << movie_resp
-    end 
-    render json: movie_arr
+    @movies = Movie.all
+    render json: @movies
   end
 end
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ApplicationRecord
+  def self.import
+    (1..100).each do #|page|
+      movie_resp = HTTP.get("https://api.themoviedb.org/3/discover/movie?api_key=GOES HERE&language=en-US&sort_by=release_date.desc&page=#{page}&with_genres=27")
+      movie_resp.each do #|movie|
+        Movie.create(
+          original_title: movie['original_title'],
+          overview: movie['overview'],
+          poster_path: movie['poster_path'],
+          tagline: movie['tagline'],
+        )
+      end
+    end 
+  end 
 end
